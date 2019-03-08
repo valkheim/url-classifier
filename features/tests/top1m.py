726df0591c13177baae0c9a28c26a889114955a6
Charles Paulet <charles.paulet@epitech.eu>
2019-03-08 18:03:19 +0100

    test: add BadValue tests on top1m feature

--- features/tests/top1m.py
+++ features/tests/top1m.py
@@ -11,6 +11,10 @@ class TestTop1M(unittest.TestCase):
     def test_parameters(self):
         self.assertEqual(top1m.get(""), 0)
 
+    def test_badValues(self):
+        self.assertEqual(top1m.get("google.com"), 0)
+        self.assertEqual(top1m.get("http://google.non_existent_tld"), 0)
+
 
 if __name__ == "__main__":
     unittest.main()
