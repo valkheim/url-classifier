0c54887d0f30c77043729a58e1fb2d84889b958c
Charles Paulet <charles.paulet@epitech.eu>
2019-03-10 00:05:51 +0100

    feat: add classifiers comparison

--- data/processor.py
+++ data/processor.py
@@ -10,17 +10,17 @@ class Processor(data.Data):
         if dataset_fname:
             print("Load dataset",dataset_fname)
         else:
-            self._generate_dataset()
+            self._generate_dataset(100)
         self._save_dataset()
 
         #next implems
+        #preprocess, sanitize, handle features.get() errors
         #self._save_dataset() # write dataset to csv
         #self._load_dataset() # load past dataset
 
-    def _generate_dataset(self):
-        print("Generate url dataset")
+    def _generate_dataset(self, size):
         """
-            iterate through safe/unsafe lists
+            iterate through safe/unsafe lists of length 'size'
             calculate features scores
             add new row to dataset, eg:
             [0, 16, 432]
@@ -30,10 +30,15 @@ class Processor(data.Data):
              +-- label (0: safe, 1: unsafe)
         """
         dataset = []
-        for url in self.get_data()["unsafe"][:100]:
+        unsafe = self.get_data()["unsafe"]
+        safe = self.get_data()["safe"]
+        balance = unsafe.size if len(safe) > unsafe.size else len(safe)
+        balance = size if balance > size else balance
+        print("Generate url dataset of size 2 *", balance)
+        for url in unsafe[:balance]:
             fn = features.Features(url)
             dataset.append(fn.get(1))
-        for url in self.get_data()["safe"][:100]:
+        for url in safe[:balance]:
             fn = features.Features(url)
             dataset.append(fn.get(0))
         self._df = pd.DataFrame(dataset, columns=features.Features().get_header())
@@ -42,3 +47,9 @@ class Processor(data.Data):
         print("Save url dataset")
         path = "./data/dataset.csv"
         self._df.to_csv(path, sep=',', encoding='utf-8', header=True, index=False)
+
+    def get(self):
+        header = features.Features().get_header()
+        X = self._df[header[1::]].to_numpy().astype(float)
+        y = self._df[header[0]].to_numpy().astype(float)
+        return X, y
--- puc.py
+++ puc.py
@@ -5,7 +5,9 @@ from features import top1m
 
 if __name__ == "__main__":
     p = processor.Processor()
+    X, y = p.get()
     v = visualizer.Visualizer()
-    v.scatter_matrix("./data/dataset.csv")
+    #v.scatter_matrix("./data/dataset.csv")
+    #v.compare_classifiers(X, y)
     #d = p.get_data()
     #print(top1m.get("http://google.com", d))
