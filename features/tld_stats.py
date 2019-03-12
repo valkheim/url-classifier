c10c193e68059e942634bb9ae5b6298863159f32
Charles Paulet <charles.paulet@epitech.eu>
2019-03-12 15:18:59 +0100

    fix: tld usage

--- features/tld_stats.py
+++ features/tld_stats.py
@@ -3,7 +3,7 @@ import tldextract
 
 def get(address, data):
     cache_extract = tldextract.TLDExtract(cache_file="./data/tld.cache")
-    tld = cache_extract(address)
+    tld = cache_extract(address).suffix
     try:
         return data["tld-stats"][tld]
     except BaseException:
